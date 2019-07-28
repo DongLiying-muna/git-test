6327d72dfc99f1818651db1b718b01bfcb3a9dc1
DLY <18837563837@163.com>
2019-07-28 17:30:33 +0800

    主分支第二次提交

--- src/com/itheima/git/GitTest.java
+++ src/com/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是主分支第一次提交");
+        System.out.println("我是主分支第二次提交");
     }
 }
