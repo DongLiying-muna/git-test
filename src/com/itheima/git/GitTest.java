2ccb1e7a9f40f959bbc8dc90279d423a1f9e4989
DLY <18837563837@163.com>
2019-07-28 17:40:11 +0800

    同事第一次提交

--- src/com/itheima/git/GitTest.java
+++ src/com/itheima/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是主分支第一次提交");
         System.out.println("我是主分支第二次提交");
+        System.out.println("我是你的同事，我这是第一次提交");
+
     }
 }
